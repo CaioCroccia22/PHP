460a5c73281ca3e5e65a6a49e310c036fca1ef06
caiocroccia22 <caiocroccia22@gmail.com>
2024-01-30 17:49:10 -0300

    Olá mundo

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
-<?php
-    //Codigo PHP
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 1</title>
+</head>
+<body>
+    <?php
+        //Escrevendo na tela
+        echo "Olá mundo!";
 
-?>
+    ?>
 
 
-<?//Codigo ?>
+    <?//Codigo ?>
+</body>
+</html>
+
